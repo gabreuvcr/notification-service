c2858089bdb29bf6040c4b629792f8350099117b
Gabriel Rocha <gabreuvcr@gmail.com>
2022-12-14 00:35:42 -0300

    Added repository and use-cases in infra

---
+++ src/infra/http/controllers/notifications.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Body, Post } from '@nestjs/common';
+import { SendNotification } from 'src/application/use-cases/send-notification';
+import { CreateNotificationBody } from '../dtos/create-notification-body';
+
+@Controller('notifications')
+export class NotificationsController {
+  constructor(private sendNotificaiton: SendNotification) {}
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
+
+    const { notification } = await this.sendNotificaiton.execute({
+      recipientId,
+      content,
+      category
+    });
+
+    return { notification };
+  }
+}
