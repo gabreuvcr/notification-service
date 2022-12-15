b6cee395d93e0be8bcf83a2b5515c8610a0e0163
Gabriel Rocha <gabreuvcr@gmail.com>
2022-12-15 03:03:45 -0300

    Added more use cases

---
+++ src/application/use-cases/count-recipient-notifications.spec.ts
@@ -0,0 +1,42 @@
+import { Content } from "@application/entities/content";
+import { Notification } from "@application/entities/notification";
+import { InMemoryNotificationsRepository } from "@test/repositories/in-memory-notifications-repository";
+import { CancelNotification } from "./cancel-notification";
+import { CountRecipientNotifications } from "./count-recipient-notifications";
+
+describe('Count recipient notification', () => {
+  it('should be able to count recpieint notifications', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const countRecipientNotifications = new CountRecipientNotifications(notificationsRepository);
+
+    await notificationsRepository.create(
+      new Notification({
+        category: 'social',
+        content: new Content("Nova solicitação de amizade."),
+        recipientId: 'recipient-1',
+      })
+    );
+
+    await notificationsRepository.create(
+      new Notification({
+        category: 'social',
+        content: new Content("Nova solicitação de amizade."),
+        recipientId: 'recipient-1',
+      })
+    );
+
+    await notificationsRepository.create(
+      new Notification({
+        category: 'social',
+        content: new Content("Nova solicitação de amizade."),
+        recipientId: 'recipient-2',
+      })
+    );
+
+    const { count } = await countRecipientNotifications.execute({
+      recipientId: 'recipient-1'
+    });
+
+    expect(count).toEqual(2);
+  });
+});
